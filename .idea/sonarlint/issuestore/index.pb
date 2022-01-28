2a05ca69e679d7230f588c61e16051be5e4e8588
Rahul Kumar <rahulkumar060995@gmail.com>
2022-01-28 09:32:31 +0530

    Time Complexity

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,3 @@
 
-s
-Csrc/main/java/org/practice/cpdsa/binarytree/PostOrderTraversal.java,5\4\5490435c1f262513645b228f2b00ed8e6886cfed
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
--- src/main/java/org/practice/cpdsa/CPDSAApplication.java
+++ src/main/java/org/practice/cpdsa/CPDSAApplication.java
@@ -6,7 +6,14 @@ public class CPDSAApplication {
        System.console();
     }
 
+    // Time Complexity for Data Structure :-
     // 1. for calculation of time complexity always consider higher order do not consider lower order like:
     // n^2 + n or 100 -> n^2 and n * m -> nm and n + n -> n
-    // 2. if loop is increasing by 2 then it will be o(logN)
+    // O(1) < O(N) < O(N^2) < O(N^3)
+    // O(N^2) + 1000 = O(N^2) for large input kind of million records we can neglect 1000
+    // O(N^2) + N = O(N^2) for large input kind of million records we can neglect N
+    // if we have two loops one is running n times and other is running M times O(N + M) Additive complexity
+    // if we have two loops one inside another with m and n size then O(NM)
+    // if one loop is running n times and inside that loop is running n/2 times then O(N*N/2) = O(N^2) division of 2 does not matter.
+    // if any loop is increasing by 2 then it will be o(logN)
 }
