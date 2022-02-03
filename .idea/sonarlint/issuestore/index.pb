7e9ae74557465fff493a95391133ff9778714f61
Rahul Kumar <rahulkumar060995@gmail.com>
2022-02-03 07:52:09 +0530

    Leet Code Problems Array

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,11 @@
 
 9
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+e
+5src/main/java/org/practice/cpdsa/linkedlist/Node.java,a\5\a530d09073881dfeb77ba5a8ffc5b600eab9522c
+f
+6src/main/java/org/practice/cpdsa/heaps/KthMaximum.java,9\b\9b80f7a762c16a7b8f492b72bfed9609c11697a9
+q
+Asrc/main/java/org/practice/cpdsa/linkedlist/CustomLinkedList.java,0\1\0100707d488b4f2751140c297d9b85b4e3771c8a
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
---
+++ src/main/java/org/practice/cpdsa/leetcode/TwoSum.java
@@ -0,0 +1,41 @@
+package org.practice.cpdsa.leetcode;
+
+import java.util.*;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter The size of array :- ");
+        int size = scanner.nextInt();
+        System.out.print("Enter The target :- ");
+        int target = scanner.nextInt();
+        int[] numArray = new int[size];
+        System.out.println("Enter The elements of array :- ");
+        for(int i = 0; i < size; i++) {
+            numArray[i] = scanner.nextInt();
+        }
+        int[] output = twoSum(numArray, target);
+        System.out.println("The third Maximum number is :- " + Arrays.toString(output));
+        scanner.close();
+    }
+
+    private static int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int[] output = new int[2];
+
+        for(int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if(numMap.containsKey(diff)) {
+                output[0] = numMap.get(diff);
+                output[1] = i;
+                break;
+            } else {
+                numMap.put(nums[i], i);
+            }
+        }
+
+        return output;
+    }
+}
