2feca07ab6423bc57d209e05187021943ea7bd70
Rahul Kumar <rahulkumar060995@gmail.com>
2022-06-08 17:09:02 +0530

    Update CarPooling.java

--- src/main/java/org/practice/cpdsa/array/miscellaneous/CarPooling.java
+++ src/main/java/org/practice/cpdsa/array/miscellaneous/CarPooling.java
@@ -17,4 +17,39 @@ public class CarPooling {
         System.out.println(result.get(true));
         System.out.println(result.get(false));
     }
+
+    // https://www.codingninjas.com/codestudio/library/car-pooling
+    //
+
+    public boolean carPooling(int[][] trips, int capacity) {
+        int[] arr = new int[1000];
+        for(int i=0; i < trips.length; i++){
+            for(int j = trips[i][1]; j < trips[i][2]; j++){
+                arr[j] += trips[i][0];
+                if(arr[j] > capacity) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    public boolean carPooling2(int[][] trips, int capacity) {
+        int[] person = new int[10001];
+
+        for(int[] t : trips) {
+            person[t[1]] += t[0];
+            person[t[2]] -= t[0];
+        }
+
+        for(int num : person) {
+            capacity -= num;
+            if(capacity < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
