25a46efb585c0fe5d74e1cd6f6e5dbf1d1ddedbc
Rahul Kumar <rahulkumar060995@gmail.com>
2022-06-10 23:18:08 +0530

    Update BuildingsReceivingSunlight.java

--- src/main/java/org/practice/cpdsa/array/miscellaneous/BuildingsReceivingSunlight.java
+++ src/main/java/org/practice/cpdsa/array/miscellaneous/BuildingsReceivingSunlight.java
@@ -2,23 +2,19 @@ package org.practice.cpdsa.array.miscellaneous;
 
 public class BuildingsReceivingSunlight {
 
-    public static int longest(int[] arr, int n) {
+    public static int longest(int[] arr) {
 
         int maxStep = arr[0];
         int count = 1;
 
         for(int i = 1; i < arr.length; i++) {
 
-
             if(arr[i] >= maxStep) {
                 maxStep = arr[i];
                 count++;
             }
-
-
         }
 
         return count;
-
     }
 }
