7d583d6e6c96a810ec77d803f5495a1937690ac1
Rahul Kumar <rahulkumar060995@gmail.com>
2022-06-10 23:30:21 +0530

    Update FindDuplicatesInArray.java

--- src/main/java/org/practice/cpdsa/array/miscellaneous/FindDuplicatesInArray.java
+++ src/main/java/org/practice/cpdsa/array/miscellaneous/FindDuplicatesInArray.java
@@ -1,6 +1,23 @@
 package org.practice.cpdsa.array.miscellaneous;
 
+import java.util.*;
+
 public class FindDuplicatesInArray {
 
-    
+    public List<Integer> findDuplicates(int[] nums) {
+
+        List<Integer> data = new ArrayList<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            // to find the index
+            int index = Math.abs(nums[i]) - 1;
+
+            if(nums[index] < 0) data.add(Math.abs(nums[i]));
+
+            nums[index] = -nums[index];
+        }
+
+        return data;
+
+    }
 }
