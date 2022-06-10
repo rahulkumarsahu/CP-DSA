3e52ee5da5ab15ae07458000acb679f9d70dc743
Rahul Kumar <rahulkumar060995@gmail.com>
2022-06-11 00:13:16 +0530

    Update LargestPerimeterTriangle.java

--- src/main/java/org/practice/cpdsa/array/miscellaneous/LargestPerimeterTriangle.java
+++ src/main/java/org/practice/cpdsa/array/miscellaneous/LargestPerimeterTriangle.java
@@ -4,6 +4,13 @@ import java.util.Arrays;
 
 public class LargestPerimeterTriangle {
 
+    /**
+     * We sort the A
+     * Try to get a triangle with 3 biggest numbers.
+     * If A[n-1] < A[n-2] + A[n-3], we get a triangle.
+     * If A[n-1] >= A[n-2] + A[n-3] >= A[i] + A[j], we cannot get any triangle with A[n-1]
+     * repeat step2 and step3 with the left numbers.
+     */
     public int largestPerimeter(int[] nums) {
 
         Arrays.sort(nums);
