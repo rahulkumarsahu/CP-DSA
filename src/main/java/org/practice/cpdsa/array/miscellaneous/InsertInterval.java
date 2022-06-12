971cb33a99fe2c0d371db23d4d626bab99ca42f4
Rahul Kumar <rahulkumar060995@gmail.com>
2022-06-12 18:52:16 +0530

    Update InsertInterval.java

--- src/main/java/org/practice/cpdsa/array/miscellaneous/InsertInterval.java
+++ src/main/java/org/practice/cpdsa/array/miscellaneous/InsertInterval.java
@@ -1,19 +1,41 @@
 package org.practice.cpdsa.array.miscellaneous;
 
 
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class InsertInterval {
 
-    public static void main(String[] args) {
-        int[][] intervals = new int[][]{{1, 3},{6, 9}};
-        int[] newInterval = new int[]{2, 5};
-        int[][] output = insert(intervals, newInterval);
-        System.out.println(Arrays.deepToString(output));
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
 
+        List<Interval> result = new LinkedList<>();
+        int i = 0;
+        // add all the intervals ending before newInterval starts
+        while (i < intervals.size() && intervals.get(i).end < newInterval.start)
+            result.add(intervals.get(i++));
+        // merge all overlapping intervals to one considering newInterval
+        while (i < intervals.size() && intervals.get(i).start <= newInterval.end) {
+            newInterval = new Interval( // we could mutate newInterval here also
+                    Math.min(newInterval.start, intervals.get(i).start),
+                    Math.max(newInterval.end, intervals.get(i).end));
+            i++;
+        }
+        result.add(newInterval); // add the union of intervals we got
+        // add all the rest
+        while (i < intervals.size()) result.add(intervals.get(i++));
+        return result;
     }
 
-    public static int[][] insert(int[][] intervals, int[] newInterval) {
-       return null;
+}
+
+class Interval {
+
+    public int start;
+    public int end;
+
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
     }
+
 }
